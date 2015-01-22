fb8397cf3219658ee486c1bf1548d3ca2d53545c
2090566c <2090566c@student.gla.ac.uk>
2015-01-22 19:37:52 +0000

    Chapter 7 - Completed

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from models import Category
+from models import Page
 from django.http import HttpResponse
 
+def category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
+
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
+
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
