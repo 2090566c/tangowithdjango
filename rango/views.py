e1050a32a0cd9cc5ab457216a212a26e0382b7cc
2090566c <2090566c@student.gla.ac.uk>
2015-03-02 18:33:20 +0000

    Chapter 15 - Completed

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
+from rango.bing_search import run_query
 
 def category(request, category_name_slug):
 
@@ -181,3 +182,17 @@ def restricted(request):
 #    logout(request)
 #
 #    return HttpResponseRedirect('/rango/')
+
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
